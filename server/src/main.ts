8aadf28dcb93dc7f720c14a2753334ea0052e49a
cavaldos <khanhnguyen297kn@gmail.com>
2023-09-08 08:43:37 +0700

    fix port

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') ; // Lấy giá trị cấu hình cổng (port) hoặc sử dụng cổng mặc định 3000
+  const port = configService.get<number>('PORT') || 3000 ; // Lấy giá trị cấu hình cổng (port) hoặc sử dụng cổng mặc định 3000
   await app.listen(port, () => console.log(`Server started on port ${port}`));
 }
 
