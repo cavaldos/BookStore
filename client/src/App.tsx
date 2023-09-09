8d5ad498a15ef79d0da26b1a85b395cc9177f330
cavaldos <khanhnguyen297kn@gmail.com>
2023-09-09 11:32:32 +0700

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <h1 className="text- first-letter:backdrop:first: bg-blend-color-dodge">
         sdfsdsfadfsda
         <div className="dark:bg-dark light:bg-light">
-          <h1 className="dark:text-white light:text-black">Hello, World!</h1>
+          <h1 className="dark:text-white light:text-black">Hello, World!!!!!!!!!!!!!!</h1>
           <p className="dark:text-gray-300 light:text-gray-700">
             Lorem ipsum dolor sit amet...
           </p>
