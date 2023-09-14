3d41d851a7a2aabc0e47a84159a1e0e675e5f434
cavaldos <khanhnguyen297kn@gmail.com>
2023-09-14 22:07:54 +0700

    init

---
+++ nodejs/config/connectdb.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const URL =
+  process.env.MONGODB ||
+  "mongodb+srv://Khanh:21126072@khanh.spi9com.mongodb.net/BookStore?retryWrites=true&w=majority";
+async function connect() {
+  try {
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`connect successfully: "${URL}"`);
+  } catch (err) {
+    console.log("connect false \n", err);
+  }
+}
+module.exports = { connect };
---
+++ nodejs/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+const dotenv = require("dotenv");
+const MongoDB = require("./config/connectdb");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.use(express.json());
+dotenv.config();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+MongoDB.connect();
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+const port = process.env.PORT || 5000;
+//ROUTES
+const server = require("http").createServer(app);
+server.listen(port, () => {
+  console.log("Server is running on port", port);
+});
