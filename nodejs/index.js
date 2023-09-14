f5ecc37feda62ab47e10c23236a87a9a345a11d4
cavaldos <khanhnguyen297kn@gmail.com>
2023-09-14 22:23:27 +0700

    init

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+const dotenv = require("dotenv");
+// const MongoDB = require("./config/connectdb");
 const cors = require("cors");
 const morgan = require("morgan");
 
@@ -8,19 +10,18 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 app.use(express.json());
+dotenv.config();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+// MongoDB.connect();
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  console.log("Hello World");
-  res.json({ message: "Welcome to thase server!adsfdsf" });
+  res.json({ message: "Welcome to BookStore application." });
 });
-
-app.get("/hi", (req, res) => {
-  console.log("Hello World");
-  res.json({ message: "ihihiihiihihihiihihigadgad!" });
-});
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// const port = process.env.PORT || 5000;
+//ROUTES
+const server = require("http").createServer(app);
+server.listen(5000, () => {
+  console.log("Server is running on port", 5000);
 });
